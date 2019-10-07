c0c4d87dae0b3ec6baeac03522afc7caf9a52aa4
Nick Desmond <nick.desmond@nudatasecurity.com>
2019-10-06 20:32:12 -0700

    Cleaning the data in the ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -29,7 +29,37 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    """
+    Applies transformations to the disaster dataset to prepare it for the ML code
+    :param df: A pandas dataframe of disaster responses
+    :return: A cleaned pandas dataframe
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [x[:-2] for x in row]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = [int(x.split('-')[1]) for x in categories[column]]
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.merge(categories, left_index=True, right_index=True)
+
+    # drop duplicates
+    df = df.loc[~df.duplicated().values]
+
+    return df
 
 
 def save_data(df, database_filename):
@@ -47,7 +77,6 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-
 
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
